6fe22cb614c073ef63fde063094faba3d2120b18
Stepkin Alexander <stepkin@speechpro.com>
2022-10-27 19:04:30 +0300

    init

---
+++ src/components/MyButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="btn" type="button">
+    <span> Click Me </span>
+  </button>
+</template>
+<script>
+export default {
+  name: "my-button",
+};
+</script>
+<style scoped>
+.btn {
+  width: 200px;
+  height: 100px;
+  border: solid 1px rgb(102, 88, 116);
+  background-color: rgb(168, 142, 194);
+  border-radius: 4px;
+  color: rgb(29, 26, 26 0.5);
+  font-size: 26px;
+  font-weight: 400;
+}
+.btn:hover {
+  background-color: rgb(163, 116, 211);
+}
+.btn:active {
+  transform: translateY(2px);
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <my-button @click="clickHandler" />
+  </div>
+</template>
+<script>
+import MyButton from "../components/MyButton.vue";
+export default {
+  name: "main-page",
+  components: {
+    MyButton,
+  },
+  methods: {
+    clickHandler() {
+      alert("Thanks for click =)");
+    },
+  },
+};
+</script>
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(94, 83, 71);
+}
+</style>
